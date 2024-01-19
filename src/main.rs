cb6d496cc8cb8ab4b949b12c5afb20451bbc1342
Tyler Gregorcyk <Tyler@Gregorcyk.com>
2024-01-19 17:26:18 -0600

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -7,10 +10,24 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_stream(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+fn handle_stream(mut stream: TcpStream) {
+    let response = b"HTTP/1.1 200 OK\r\n\r\n";
+
+    match stream.write(response) {
+        Ok(_) => {
+            stream.flush().unwrap();
+        }
+        Err(err) => {
+            eprintln!("Error writing to stream: {}", err);
         }
     }
 }
